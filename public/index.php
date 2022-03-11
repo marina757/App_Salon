b859536f0a5292cf5028c25dbbcf9a72561c2683
marina <marinamarrina@hotmail.com>
2022-03-11 17:02:51 +0100

    routing e controller per autentication

--- public/index.php
+++ public/index.php
@@ -3,10 +3,25 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
+use Controllers\LoginController;
 
 $router = new Router();
 
+//Iniciar sesion
+$router->get('/', [LoginController::class, 'login']);
+$router->post('/', [LoginController::class, 'login']);
+$router->get('/logout', [LoginController::class, 'logout']);
 
+//Recuperar Password
+$router->get('/olvide', [LoginController::class, 'olvide']);
+$router->post('/olvide', [LoginController::class, 'olvide']);
+$router->get('/recuperar', [LoginController::class, 'recuperar']);
+$router->post('/recuperar', [LoginController::class, 'recuperar']);
+
+
+//Crear cuenta
+$router->get('/crear-cuenta', [LoginController::class, 'crear']);
+$router->post('/crear-cuenta', [LoginController::class, 'crear']);
 
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
