24fa194a4f075850bca4445568b5921a2cc55240
marina <marinamarrina@hotmail.com>
2022-03-15 18:22:06 +0100

    confermare account user per email

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -77,4 +77,17 @@ class LoginController {
     public static function mensaje(Router $router) {
         $router->render('auth/mensaje');
     }
+
+    public static function confirmar(Router $router) {
+        $alertas = [];
+
+        $token = s($_GET['token']);
+
+        $usuario = Usuario::where('token', $token);
+        debuguear($usuario);
+
+        $router->render('auth/confirmar-cuenta', [
+            'alertas' => $alertas
+       ]);
+    }
 }
--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -123,6 +123,13 @@ class ActiveRecord {
         return array_shift( $resultado ) ;
     }
 
+    public static function where($columna, $valor) {
+        $query = "SELECT * FROM " . static::$tabla  ." WHERE ${columna} = '${valor}'";
+        //debuguear($query);
+        $resultado = self::consultarSQL($query);
+        return array_shift( $resultado ) ;
+    }
+
     // crea un nuevo registro
     public function crear() {
         // Sanitizar los datos
