2289ac66763fc939e642bef6429525a0659e9db7
marina <marinamarrina@hotmail.com>
2022-03-15 15:16:00 +0100

    altri

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -47,6 +47,9 @@ class LoginController {
                }else {
                    //Hashear el password
                    $usuario->hashPassword();
+
+                   //Generar un token unico
+                   $usuario->crearToken();
                    debuguear($usuario);
                }
             }
--- models/Usuario.php
+++ models/Usuario.php
@@ -66,4 +66,8 @@ class Usuario extends ActiveRecord {
     public function hashPassword() {
         $this->password = password_hash($this->password, PASSWORD_BCRYPT);
     }
+
+    public function crearToken() {
+        $this->token = uniqid();
+    }
 }
