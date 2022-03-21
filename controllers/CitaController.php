abe764149bf087d222d7f44c987c6efcbe692928
marina <marinamarrina@hotmail.com>
2022-03-21 18:42:49 +0100

    mostrare il name del user autenticato

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -6,7 +6,11 @@ use MVC\Router;
 
 class CitaController {
     public static function index( Router $router) {
+        //session_start();
+
         $router->render('cita/index', [
+
+            'nombre' => $_SESSION['nombre']
 
         ]);
     }
--- views/cita/index.php
+++ views/cita/index.php
@@ -1,2 +1,48 @@
 <h1 class="nombre-pagina">Crear Nueva Cita</h1>
-<p class="descripcion-pagina">Elige tus servicios a continuacion</p>
+<p class="descripcion-pagina">Elige tus servicios y coloca tus datos</p>
+
+<div id="app">
+    <div id="paso-1" class="seccion">
+        <h2>Servicios</h2>
+        <p class="text-center">Elige tus servicios a continuacion</p>
+    <div id="servicios" class="listado-servicios"></div>
+    </div>
+    <div id="paso-2" class="seccion">
+        <h2>Tus Datos y Cita</h2>
+        <p class="text-center">Coloca tus datos y fechas de tu cita</p>
+
+        <form class="formulario">
+            <div class="campo">
+                <label for="nombre">Nombre</label>
+                <input 
+                id="nombre"
+                type="text"
+                placeholder="Tu Nombre"
+                value="<?php echo $nombre; ?>"
+                disabled
+                >
+            </div>
+
+            <div class="campo">
+                <label for="fecha">Fecha</label>
+                <input 
+                id="fecha"
+                type="date"
+                >
+            </div>
+
+            <div class="campo">
+                <label for="hora">Hora</label>
+                <input 
+                id="hora"
+                type="time"
+                >
+            </div>
+
+        </form>
+    </div>
+    <div id="paso-3" class="seccion">
+        <h2>Resumen</h2>
+        <p class="text-center">Verifica que la informacion sea correcta</p>        
+    </div>
+</div>
