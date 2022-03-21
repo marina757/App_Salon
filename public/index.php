13512c9037ff55214cd13961e214f0409c715b4f
marina <marinamarrina@hotmail.com>
2022-03-21 17:57:27 +0100

    routing e controllers per citas

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
+use Controllers\CitaController;
 use Controllers\LoginController;
 
 $router = new Router();
@@ -28,6 +29,9 @@ $router->post('/crear-cuenta', [LoginController::class, 'crear']);
 $router->get('/confirmar-cuenta', [LoginController::class, 'confirmar']);
 $router->get('/mensaje', [LoginController::class, 'mensaje']);
 
+
+//Area privada
+$router->get('/cita', [CitaController::class, 'index']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
