c82dc28b9c2ec727ba6246eec1a022ab7efe9d22
marina <marinamarrina@hotmail.com>
2022-03-22 11:22:36 +0100

    naveg per tabs

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,25 @@
-alert('desde');
+let paso = 1;
+
+document.addEventListener('DOMContentLoaded', function() {
+    iniciarApp();
+});
+
+function iniciarApp() {
+    
+    tabs(); //Cambia la seccion cuando se presionan tabs
+}
+
+function mostrarSeccion() {
+    console.log('mostrando seccion...');
+}
+
+function tabs() {
+    const botones = document.querySelectorAll('.tabs button');
+
+    botones.forEach( boton => {
+        boton.addEventListener('click', function(e) {
+           paso = parseInt( e.target.dataset.paso );
+           mostrarSeccion();
+        });
+    })
+}
