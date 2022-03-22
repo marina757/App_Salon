6da7a89c019bd1378e8278dd3345ddd0bf15fffd
marina <marinamarrina@hotmail.com>
2022-03-22 13:42:01 +0100

    navigation tabs, pagination

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,43 @@
 let paso = 1;
+const pasoInicial = 1;
+const pasoFinal = 3;
 
 document.addEventListener('DOMContentLoaded', function() {
     iniciarApp();
 });
 
 function iniciarApp() {
-    
+    mostrarSeccion(); //Muestra y oculta las secciones
     tabs(); //Cambia la seccion cuando se presionan tabs
+
+    botonesPaginador(); //agrega o quita los botones del paginador
+
+    paginaSiguiente();
+    paginaAnterior();
 }
 
 function mostrarSeccion() {
-    console.log('mostrando seccion...');
+    //ocultar la seccion que tenga clase mostrar
+    const seccionAnterior = document.querySelector('.mostrar');
+    if (seccionAnterior) {
+        seccionAnterior.classList.remove('mostrar');
+    }
+
+   //Seleccionar la seccion con el paso
+   const pasoSelector = `#paso-${paso}`;
+   const seccion = document.querySelector(pasoSelector);
+   seccion.classList.add('mostrar');
+
+
+   //Quita la clase de actual al tab anterior
+   const tabAnterior = document.querySelector('.actual');
+   if (tabAnterior) {
+       tabAnterior.classList.remove('actual');
+   }
+
+   //Resaltar el tab actual
+   const tab = document.querySelector(`[data-paso="${paso}"]`);
+   tab.classList.add('actual');
 }
 
 function tabs() {
@@ -20,6 +47,45 @@ function tabs() {
         boton.addEventListener('click', function(e) {
            paso = parseInt( e.target.dataset.paso );
            mostrarSeccion();
+
+           botonesPaginador(); 
         });
+    })
+}
+
+function botonesPaginador() {
+    const paginaAnterior = document.querySelector('#anterior');
+    const paginaSiguiente = document.querySelector('#siguiente');
+
+    if (paso === 1) {
+        paginaAnterior.classList.add('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+    }else if (paso === 3) {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.add('ocultar');
+    }else {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+    }
+    mostrarSeccion();
+}
+
+function paginaAnterior() {
+    const paginaAnterior = document.querySelector('#anterior');
+    paginaAnterior.addEventListener('click', function() {
+        if (paso <= pasoInicial) return;
+        paso--;
+        
+        botonesPaginador();
+    })
+}
+
+function paginaSiguiente() {
+    const paginaSiguiente = document.querySelector('#siguiente');
+    paginaSiguiente.addEventListener('click', function() {
+        if (paso >= pasoFinal) return;
+        paso++;
+        
+        botonesPaginador();
     })
 }
