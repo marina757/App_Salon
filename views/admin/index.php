d1fcbb7899cdd37288c25bd64016a78258511377
marina <marinamarrina@hotmail.com>
2022-04-06 12:01:18 +0200

    citas schermo

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,21 +1,41 @@
 <h1 class="nombre-pagina">Panel de Administracion</h1>
 
-<?php 
-    include_once __DIR__ . '/../templates/barra.php' 
+<?php
+include_once __DIR__ . '/../templates/barra.php'
 ?>
 
 <h2>Buscar Citas</h2>
 <div class="busqueda">
     <form class="formulario">
-            <div class="campo">
-                <label for="fecha">Fecha</label>
-                <input 
-                    type="date"
-                    id="fecha"
-                    name="fecha"
-                >
-            </div>
+        <div class="campo">
+            <label for="fecha">Fecha</label>
+            <input type="date" id="fecha" name="fecha">
+        </div>
     </form>
 </div>
 
-<div id="citas-admin"></div>
+<div id="citas-admin">
+    <ul class="citas">
+        <?php
+        $idCita = 0;
+        foreach ($citas as $cita) {
+            if ($idCita !== $cita->id) {
+
+        ?>
+                <li>
+                    <p>ID: <span><?php echo $cita->id; ?></span></p>
+                    <p>Hora: <span><?php echo $cita->hora; ?></span></p>
+                    <p>Cliente: <span><?php echo $cita->cliente; ?></span></p>
+                    <p>Email: <span><?php echo $cita->email; ?></span></p>
+                    <p>Telefono: <span><?php echo $cita->telefono; ?></span></p>
+
+                    <h3>Servicios</h3>
+                <?php
+                $idCita = $cita->id;
+            } //fin de if ?>
+            <p class="servicio"><?php echo $cita->servicio . " " . $cita->precio; ?></p>
+                </li>
+            <?php } //fin de foreach
+            ?>
+    </ul>
+</div>
