7c447b57bd975f5adbee699fb82e9f97682e2301
marina <marinamarrina@hotmail.com>
2022-04-12 16:53:53 +0200

    css servicios, altri

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -7,8 +7,11 @@ use Model\Servicio;
 
  class ServicioController {
      public static function index(Router $router) {
+
+        $servicios = Servicio::all();
         $router->render('servicios/index', [
-            'nombre' => $_SESSION['nombre']
+            'nombre' => $_SESSION['nombre'],
+            'servicios' => $servicios
         ]);
      }
 
