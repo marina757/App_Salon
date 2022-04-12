056613ae701db2f642ce222c702386d226cbe35e
marina <marinamarrina@hotmail.com>
2022-04-12 15:29:02 +0200

    form per servicios

--- views/servicios/crear.php
+++ views/servicios/crear.php
@@ -4,3 +4,9 @@
 <?php
     include_once __DIR__ . '/../templates/barra.php';
 ?>
+
+<form action="/servicios/crear" method="POST" class="formulario">
+
+    <?php include_once __DIR__ . '/formulario.php'; ?>
+    <input type="submit" class="boton" value="Guardar servicio">
+</form>
