70ebec484d4633a19a03643d6b135e23d1405f37
marina <marinamarrina@hotmail.com>
2022-04-12 14:29:35 +0200

    menu admin

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -3,3 +3,13 @@
 
     <a class="boton" href="/logout">Cerrar sesion</a>
 </div>
+
+<?php if (isset($_SESSION['admin'])) { ?>       
+    <div class="barra-servicios">
+        <a class="boton" href="/admin">Ver citas</a>
+        <a class="boton" href="/servicios">Ver servicios</a>
+        <a class="boton" href="/servicios/crear">Nuevo servicio</a>
+    </div>
+
+
+<?php } ?>
